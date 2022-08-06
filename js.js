3801ff396a6080cb7ff944c2e97dedfe197cf7cf
goroG1 <goroglng@gmail.com>
2022-08-06 13:11:40 +0700

    style

--- js.js
+++ js.js
@@ -21,9 +21,9 @@ function addBook() {
   document.dispatchEvent(new Event(RENDER_EVENT));
 }
 
-document.addEventListener(RENDER_EVENT, function () {
-  console.log(books);
-});
+// document.addEventListener(RENDER_EVENT, function () {
+//   console.log(books);
+// });
 
 function generateId() {
   return +new Date();
@@ -56,7 +56,35 @@ function makeBookList(bookList) {
   const container = document.createElement('div');
   container.classList.add('book_item', 'shadow');
   container.append(textContainer);
-  container.setAttribute('id', 'book-${bookList,id}');
+  container.setAttribute('id', `book-${bookList.id}`);
+
+  if (bookList.isComplete) {
+    const undoButton = document.createElement('button');
+    undoButton.classList.add('undo-button');
+
+    undoButton.addEventListener('click', function () {
+      undoTaskFromComplete(bookList.id);
+    });
+
+    const trashButton = document.createElement('button');
+    trashButton.classList.add('trash-button');
+
+    trashButton.addEventListener('click', function () {
+      removeTaskFromCompleted(bookList.id);
+    });
+
+    container.append(undoButton, trashButton);
+
+  } else {
+    const checkButton = document.createElement('button');
+    checkButton.classList.add('check-button');
+
+    checkButton.addEventListener('click', function () {
+      addTaskCompleted(bookList.id);
+    });
+
+    container.append(checkButton);
+  }
 
   return container;
 }
@@ -66,8 +94,36 @@ document.addEventListener(RENDER_EVENT, function () {
   const incompleteBOOKList = document.getElementById('incompleteBookshelfList');
   incompleteBOOKList.innerHTML = '';
 
+  const completeBOOKList = document.getElementById('completeBookshelfList');
+  completeBOOKList.innerHTML = '';
+
   for (const bookItem of books) {
     const bookElement = makeBookList(bookItem);
-    incompleteBOOKList.append(bookElement);
+    if (!bookItem.isComplete) {
+      incompleteBOOKList.append(bookElement);
+    }
+    else {
+      completeBOOKList.append(bookElement);
+    }
   }
 });
+
+function addTaskCompleted(bookId) {
+  const bookTarget = findBook(bookId);
+
+  if (bookTarget == null) return;
+
+  bookTarget.isCompleted = true;
+  document.dispatchEvent(new Event(RENDER_EVENT));
+}
+
+function findBook(bookId) {
+  for (const bookItem of books) {
+    if (bookItem.id === bookId) {
+      return bookItem;
+    }
+  }
+
+  return null;
+}
+
